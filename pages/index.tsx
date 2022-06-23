b9d310ccbc4d4abc5aa4048cdbb703b6a615feab
Mitch Hall <scubaphoto@gmail.com>
2022-06-23 13:15:43 -0700

    Skills complete, needs some minor adjustments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,22 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import About from '../components/About'
-import Main from '../components/Main'
-import Navbar from '../components/Navbar'
-import Skills from '../components/Skills'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import About from "../components/About";
+import Main from "../components/Main";
+import Navbar from "../components/Navbar";
+import Skills from "../components/Skills";
 
 export default function Home() {
-     return (
-       <div>
-        <Head>
-           <title>ArtificerDEVsolutions</title>
-           <link rel="icon" href="/favicon.ico" />
-         </Head>
+  return (
+    <div>
+      <Head>
+        <title>ArtificerDEVSolutions</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <Main />
       <About />
       <Skills />
-
     </div>
-  )
+  );
 }
